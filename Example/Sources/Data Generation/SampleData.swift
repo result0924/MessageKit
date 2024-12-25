29c0222c269516349d883629121520760da50ac3
Edward <edward820630@gmail.com>
2024-12-25 20:43:45 +0800

    Add reply message item and mock data in example app
    tmp
    
    tmp

--- Example/Sources/Data Generation/SampleData.swift
+++ Example/Sources/Data Generation/SampleData.swift
@@ -320,7 +320,32 @@ final internal class SampleData {
         }
         completion(messages)
     }
-    
+
+    func getReplyMessages(count: Int, completion: ([MockMessage]) -> Void) {
+        var messages: [MockMessage] = []
+        for _ in 0..<count {
+            let uniqueID = UUID().uuidString
+            let date = dateAddingRandomTime()
+
+            let type = ReplyQuoteItemType.allCases.dropLast().randomElement() ?? .photoAndText
+            var replyItem: CustomReplyMessageItem.ReplyItem
+
+            switch type {
+            case .photoAndText:
+                replyItem = CustomReplyMessageItem.ReplyItem(quoteType: .photoAndText, title: "回覆", quoteImage: UIImage(imageLiteralResourceName: "image_message_placeholder"), quoteContent: "Title.pdf", quotePhotoURL: nil, replyMessageType: .text, replyContent: "回覆訊息photoAndText", replyViewBackgroundColor: CustomReplyMessageItem.grayBackgroundColor)
+            case .photoOnly:
+                replyItem = CustomReplyMessageItem.ReplyItem(quoteType: .photoOnly, title: "回覆", quoteImage: nil, quoteContent: nil, quotePhotoURL: nil, replyMessageType: .text, replyContent: "回覆訊息photoOnly", replyViewBackgroundColor: CustomReplyMessageItem.grayBackgroundColor)
+            case .textOnly:
+                replyItem = CustomReplyMessageItem.ReplyItem(quoteType: .textOnly, title: "回覆", quoteImage: nil, quoteContent: "textOnly", quotePhotoURL: nil, replyMessageType: .sticker, replyContent: "https://dev.health2sync.com/images/stickers/1/s_014.png", replyViewBackgroundColor: .clear)
+            case .unknown:
+                replyItem = CustomReplyMessageItem.ReplyItem(quoteType: .unknown, title: "回覆", quoteImage: nil, quoteContent: "textOnly", quotePhotoURL: nil, replyMessageType: .text, replyContent: "回覆訊息unknown", replyViewBackgroundColor: CustomReplyMessageItem.grayBackgroundColor)
+            }
+            let message = MockMessage(reply: CustomReplyMessageItem(replyItem: replyItem), user: system, messageId: uniqueID, date: date)
+            messages.append(message)
+        }
+        completion(messages)
+    }
+
     func getTemplateAudioMessages(count: Int, completion: ([MockMessage]) -> Void) {
         var messages: [MockMessage] = []
         // Enable Template Messages
---
+++ Example/Sources/Models/CustomReplyMessageItem.swift
@@ -0,0 +1,114 @@
+//
+//  CustomReplyMessageItem.swift
+//  ChatExample
+//
+//  Created by Edward Chang on 2024/12/25.
+//  Copyright © 2024 MessageKit. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+struct CustomReplyMessageItem: ReplyMessageItem {
+
+    static let grayBackgroundColor = UIColor(red: 244 / 255, green: 244 / 255, blue: 244 / 255, alpha: 1)
+
+    struct ReplyItem {
+        let quoteType: ReplyQuoteItemType
+        let title: String
+        let quoteImage: UIImage?
+        let quoteContent: String?
+        let quotePhotoURL: URL?
+        let replyMessageType: ReplyMessageType
+        let replyContent: String
+        let quoteViewBackgroundColor: UIColor = grayBackgroundColor
+        let replyViewBackgroundColor: UIColor
+    }
+
+    // quote message content
+    var quoteType: MessageKit.ReplyQuoteItemType
+    let title: String
+    let quoteImage: UIImage?
+    let quoteContent: String?
+    let quotePhotoURL: URL?
+
+    // message content
+    let messageType: ReplyMessageType
+    let text: NSAttributedString
+    var mediaURL: URL?
+    let textViewContentInset: UIEdgeInsets
+    let replyTextContentInset: UIEdgeInsets
+    let textViewHeight: CGFloat
+
+    var quoteOriginY: CGFloat
+    var quoteHeight: CGFloat
+    var quoteBottomPadding: CGFloat
+    let messageHeight: CGFloat
+    let size: CGSize
+    let replyTextWidth: CGFloat
+    let quoteViewBackgroundColor: UIColor
+    var replyViewBackgroundColor: UIColor
+
+    init(replyItem: Self.ReplyItem) {
+        self.quoteType = replyItem.quoteType
+        self.title = replyItem.title
+        self.quoteImage = replyItem.quoteImage
+        self.quoteContent = replyItem.quoteContent
+        self.quotePhotoURL = replyItem.quotePhotoURL
+        self.messageType = replyItem.replyMessageType
+        self.textViewContentInset = UIEdgeInsets(top: 12, left: 14, bottom: 12, right: 14)
+        self.replyTextContentInset = UIEdgeInsets(top: 12, left: 14, bottom: 12, right: 14)
+        self.quoteViewBackgroundColor = replyItem.quoteViewBackgroundColor
+        self.replyViewBackgroundColor = replyItem.replyViewBackgroundColor
+
+        let screenWidth: CGFloat = UIScreen.main.bounds.width
+        let collectionViewLeftRightPadding: CGFloat = 95
+        let maxBubbleWidth = screenWidth - collectionViewLeftRightPadding
+        let maxTextWidth = maxBubbleWidth - textViewContentInset.left - textViewContentInset.right
+        let textSize = CGSize(width: maxTextWidth, height: CGFloat(Float.greatestFiniteMagnitude))
+
+        let contentRect: CGRect
+        let boundingRect: CGRect
+
+        let defaultText = NSAttributedString(string: "")
+
+        switch replyItem.replyMessageType {
+        case .text:
+            let attributedTextString = NSAttributedString(string: replyItem.replyContent, attributes: [.foregroundColor: UIColor.lightGray, .font: UIFont.systemFont(ofSize: 16, weight: .regular)])
+            self.text = attributedTextString
+            contentRect = attributedTextString.boundingRect(with: textSize, options: [.usesLineFragmentOrigin, .usesFontLeading], context: nil)
+            boundingRect = attributedTextString.boundingRect(with: CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.zero), options: [.usesLineFragmentOrigin], context: nil)
+        case .sticker:
+            self.text = defaultText
+            self.mediaURL = URL(string: replyItem.replyContent)
+            contentRect = CGRect(x: 0, y: 0, width: 96, height: 96)
+            boundingRect = CGRect(x: 0, y: 0, width: 96, height: 96)
+        }
+
+        var replyHeight: CGFloat = 0
+        replyHeight = contentRect.size.height + self.textViewContentInset.top + self.textViewContentInset.bottom
+
+        var quoteContentHeight: CGFloat
+
+        switch quoteType {
+        case .photoAndText:
+            quoteContentHeight = 84
+        case .textOnly:
+            let attributedMessageTypeContent = NSAttributedString.init(string: replyItem.quoteContent ?? "", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .regular)])
+            let messageTypeContentRect = attributedMessageTypeContent.boundingRect(with: textSize, options: [NSStringDrawingOptions.usesLineFragmentOrigin, NSStringDrawingOptions.usesFontLeading], context: nil)
+            quoteContentHeight = min(60 + messageTypeContentRect.size.height, 106)
+        case .photoOnly:
+            quoteContentHeight = 112
+        case .unknown:
+            quoteContentHeight = 102
+        }
+
+        self.quoteOriginY = quoteContentHeight
+        self.textViewHeight = replyHeight.rounded(.up)
+        self.quoteBottomPadding = 8
+        self.quoteHeight = quoteContentHeight
+        self.messageHeight = self.textViewHeight
+        self.replyTextWidth = ceil(boundingRect.width) + self.textViewContentInset.left + self.textViewContentInset.right
+        self.size = CGSize(width: maxBubbleWidth, height: self.quoteHeight + self.quoteBottomPadding + self.messageHeight)
+    }
+}
